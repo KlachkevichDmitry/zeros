7545577a11cede30b65c0b5a266c40b3a25c2cc9
Klachkevich Dmitry <klachkevich.dmitry@gmail.com>
2018-02-21 17:33:41 +0300

    work, but veeeeery slowly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+        var count=0;
+        var arr=[];
+
+        for (var x=5; x<=number; x+=5) {
+            arr.push(x);}
+        for (var i=0; i<arr.length; i++) {
+            while (arr[i] > 1) {
+                if (arr[i] % 5 == 0) {
+                    count++;
+                    arr[i] = arr[i] / 5;
+                } else break;
+            }
+        }
+        return count;
 }
